a8b2ab5cae757d5521300db494cacc59bc2d9081
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2023-01-25 13:47:49 -0800

    Created working city search view and template to show results

--- event_list/models.py
+++ event_list/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class EventList(models.Model):
-    short_title = models.CharField(max_length=200)
-    type = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    state = models.CharField(max_length=200)
-    postal_code = models.CharField(max_length=200)
-    datetime_utc = models.DateTimeField
+    short_title = models.CharField(max_length=200,blank=True,null=True)
+    event_type = models.CharField(max_length=200,blank=True,null=True)
+    city = models.CharField(max_length=200,blank=True,null=True)
+    state = models.CharField(max_length=200,blank=True,null=True)
+    postal_code = models.CharField(max_length=200,blank=True,null=True)
+    # datetime_utc = models.DateTimeField #it doesn't like this OR datetime_local
 
     def __str__(self):
         return self.short_title
--- event_list/views.py
+++ event_list/views.py
@@ -10,21 +10,33 @@ def get_events(request):
     all_events = {}
     if 'city' in request.GET:
         city = request.GET['city']
-        url = 'https://api.seatgeek.com/2/events?venue.city=%s&client_id=MzE2MDc3Mzl8MTY3NDUxMjYxMS42OTk4Mg' % city
+        url = 'https://api.seatgeek.com/2/events?venue.city=%s&client_id=MzE2MDc3Mzl8MTY3NDUxMjYxMS42OTk4Mg' %city
+        # url = 'https://api.seatgeek.com/2/events?venue.city=eugene&client_id=MzE2MDc3Mzl8MTY3NDUxMjYxMS42OTk4Mg'
         response = requests.get(url)
         data = response.json()
-        events = data['events'] #if this doesn't work, try venues, but events should be name of data set
+        events = data['events']
 
-        for i in events:
-            event_data = EventList(
-                short_title = i['short_title'],
-                type = i['type'],
-                city = i['city'],       
-                state = i['state'],
-                postal_code = i['postal_code'],  
-                datetime_utc = i['datetime_utc'],     
+        for i in range(len(events)):
+            event_data = EventList.objects.create(
+                short_title = events[i]['short_title'], 
+                event_type = events[i]['type'],
+                city = events[i]['venue']['city'],       
+                state = events[i]['venue']['state'],
+                postal_code = events[i]['venue']['postal_code'],  
+                #datetime_utc = events[i]['datetime_utc'],     
             )
-            event_data.save()
-            all_events = EventList.objects.all().order_by('-type')
+        #events = data['events'] 
+        # event_data = EventList.objects.create(
+        #     short_title = data['short_title'],
+        #     event_type = data['type'],
+        #     city = data['venue']['city'],       
+        #     state = data['venue']['state'],
+        #     postal_code = data['venue']['postal_code'],  
+        #     datetime_local = data['datetime_utc'],     
+        # )
 
-    return render (request, 'event_list.html', { "all_events": all_events})
+        event_data.save()
+        # all_events = EventList.objects.all().order_by('-type')
+        all_events = EventList.objects.all()
+
+    return render (request, 'event_list.html', {"all_events": all_events})
