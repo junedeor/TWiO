48accfbbc501cfbeedcf91918aaa8903cb029f89
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2023-02-08 09:55:03 -0800

    Fixed errors except multi key dict value

--- TWiO_Django/event_list/models.py
+++ TWiO_Django/event_list/models.py
@@ -10,16 +10,17 @@ class EventList(models.Model):
     lon = models.FloatField(blank=True,null=True)
     lat = models.FloatField(blank=True,null=True)
     local_dtg = models.DateTimeField(blank=True,null=True)
-    saved_searches = models.CharField(max_length=200,blank=True,null=True)
-    # recent_searches should just be named after its created dtg; should it have dtf or charfield?
-    recent_searches = models.CharField(max_length=200,blank=True,null=True)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,blank=True,null=True)
+    #author = models.ForeignKey('auth.User',on_delete=models.CASCADE,blank=True,null=True)
     # this was returning a "impossible to add w/o providing a default" error:
     # created= models.DateTimeField(auto_now_add=True)
-    created= models.DateTimeField(default=timezone.now)
-    notes = models.CharField(max_length=200,blank=True,null=True) #field for user-added notes; unsure ATT if should be attached to user or specific search
+    #created= models.DateTimeField(default=timezone.now) #no errors
+    #notes = models.CharField(max_length=200,blank=True,null=True) #field for user-added notes; unsure ATT if should be attached to user or specific search
+    # are these fields necessary here?
+    # saved_searches = models.CharField(max_length=200,blank=True,null=True)
+    # # recent_searches should just be named after its created dtg; should it have dtf or charfield?
+    # recent_searches = models.CharField(max_length=200,blank=True,null=True)
     def __str__(self):
         return self.short_title
     
-    class Meta: 
-        ordering = ['-created']
+    # class Meta: 
+    #     ordering = ['-created']
